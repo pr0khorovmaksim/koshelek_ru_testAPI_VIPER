07b24e7fd2e669882cca503634890101b46eddd6
pr0khorovmaksim <pr0khorovmaksim@yandex.ru>
2020-11-06 12:15:14 +0300

    Initial Commit

---
+++ koshelek_ru_testAPI_VIPER/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  koshelek_ru_testAPI_VIPER
+//
+//  Created by maksim on 06.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
