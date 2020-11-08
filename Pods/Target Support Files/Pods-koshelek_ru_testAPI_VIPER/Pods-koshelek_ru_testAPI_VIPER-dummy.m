643bcc128ca7e08aea625813ad36875109932a96
pr0khorovmaksim <pr0khorovmaksim@yandex.ru>
2020-11-08 19:10:54 +0300

    commit

---
+++ Pods/Target Support Files/Pods-koshelek_ru_testAPI_VIPER/Pods-koshelek_ru_testAPI_VIPER-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_koshelek_ru_testAPI_VIPER : NSObject
+@end
+@implementation PodsDummy_Pods_koshelek_ru_testAPI_VIPER
+@end
---
+++ Pods/Target Support Files/Pods-koshelek_ru_testAPI_VIPER/Pods-koshelek_ru_testAPI_VIPER-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_koshelek_ru_testAPI_VIPERVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_koshelek_ru_testAPI_VIPERVersionString[];
+
